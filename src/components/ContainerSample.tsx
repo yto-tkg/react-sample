223e71595e619495a7015e8c962e206228e73c47
yto-tkg <yuto-takagi@cybozu.co.jp>
2022-08-30 19:48:39 +0900

    ContainerSample修正

--- src/components/ContainerSample.tsx
+++ src/components/ContainerSample.tsx
@@ -1,15 +1,22 @@
-const Container = (props: { title: string; children: React.ReactElement }) => {
+import React from "react";
+
+type ContainerProps = {
+  title: string
+  children: React.ReactNode
+}
+
+const Container = (props: ContainerProps): JSX.Element => {
   const { title, children } = props
 
   return (
-    <div style={ {background: 'red'} }>
+    <div style={{ background: 'red' }}>
       <span>{title}</span>
       <div>{children}</div>
     </div>
   )
 }
 
-const Parent = () => {
+const Parent = (): JSX.Element => {
   return (
     <Container title="Hello">
       <p>ここの部分が背景色で囲まれます</p>
